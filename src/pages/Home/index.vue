f601df59af688a670db8f27d98af3e6a8b79faee
chumen <582831743@qq.com>
2022-07-19 01:05:17 +0800

    'day01-home-100%'

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-    Home
+    <TypeNav></TypeNav>
+    <List></List>
+    <Today></Today>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Brand></Brand>
+
   </div>
 </template>
 
 <script>
+import TypeNav from './TypeNav'
+import List from './List'
+import Today from './Today'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+
 export default {
   name: 'Home',
+  components:{
+    TypeNav,
+    List,
+    Today,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  }
 }
 </script>
 
